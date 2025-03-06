4b6aea7777e375892ce0c26299985fe31f0549df
SPACERNGR <simataakrae3@gmail.com>
2025-03-06 21:58:05 +0200

    user model and error messages

--- api/index.js
+++ api/index.js
@@ -2,20 +2,22 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.routes.js";
+import authRouter from "./routes/auth.route.js";
 
 dotenv.config();
 
 
-mongoose.connect(process.env.MONGO).then(() => {
+mongoose.connect("mongodb://simataamwiya8:!amstillMusiq04@new-meta-home-shard-00-00.jzgdw.mongodb.net:27017,new-meta-home-shard-00-01.jzgdw.mongodb.net:27017,new-meta-home-shard-00-02.jzgdw.mongodb.net:27017/?ssl=true&replicaSet=atlas-x3iot9-shard-0&authSource=admin&retryWrites=true&w=majority&appName=new-meta-home").then(() => {
     console.log("Connected to MongoDB!!!");
     }).catch((err) => {
-    console.log("Error: ", + err);
+    console.log("Error: ", err);
     });
 
 
 
 
 const app = express();
+app.use(express.json());
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000!!!');
@@ -23,3 +25,15 @@ app.listen(3000, () => {
 
 
 app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+
+
+app.use((err, req, res, next) => {
+  const statusCode = res.statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
