b956f3eaff96ec280d0784bb277dad9d58449be3
SPACERNGR <simataakrae3@gmail.com>
2025-04-09 14:02:42 +0200

    ProfilePage created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import SignIn from "./pages/SignIn";
 import Profile from "./pages/Profile";
 import About from "./pages/About";
 import Header from "./components/Header";
+import PrivateRoute from "./components/PrivateRoute";
 
 const App = () => {
   return (
@@ -12,7 +13,9 @@ const App = () => {
    <Header />
      <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />} />
+        </Route>
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/about" element={<About />} />
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux';
 
 const Header = () => {
+
+    const { currentUser } = useSelector((state) => state.user);
+
     return (
         <header className="bg bg-slate-200 shadow-md">
-            <div className="flex justify-between items-center max-w-6xl mx-auto p-4">
+            <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
                 <Link to="/">
                     <h1 className="text-sm sm:text-xl font-bold text-slate-900 flex flex-wrap">
                         <span className="text-slate-500">MetaHome</span>
@@ -19,12 +23,12 @@ const Header = () => {
                     />
                     <button
                         type="submit"
-                        className="bg-blue-500 text-white rounded-lg p-2"
+                        className="bg-blue-500 text-white rounded-lg p-2 cursor-pointer"
                     >
                         Search
                     </button>
                 </form>
-                <ul className="flex gap-2.5">
+                <ul className="flex gap-4">
                     <Link to="/">
                         <li className="hidden sm:inline text-slate-700 hover:underline">
                             Home
@@ -35,11 +39,19 @@ const Header = () => {
                             About
                         </li>
                     </Link>
-                    <Link to="/sign-in">
-                        <li className="text-slate-700 hover:underline">
+
+
+                    <Link to="/profile">
+                     { currentUser ? (
+                        <img src={currentUser.avatar} alt="avatar" className="rounded-full h-7 w-7 object-cover" />
+                     ):
+                     (<li className="text-slate-700 hover:underline">
                             Sign In
-                        </li>
+                        </li>)
+                     }
+                    
                     </Link>
+
                 </ul>
             </div>
 
